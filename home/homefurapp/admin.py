db1d5ad80a185ac1c371576d4401142dbed225ad
Ragav12 <vijiragav12@gmail.com>
2024-02-10 16:48:06 +0530

    first commit

---
+++ home/homefurapp/admin.py
@@ -0,0 +1,14 @@
+# admin.py
+
+from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Category,Register,Product,Invoice
+
+admin.site.register(Register)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Invoice)
+
+
+
+
